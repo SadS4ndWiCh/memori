f4ff9b81a6ef300301967acd303605f0c97ca89d
sads4ndwich <caiocamargo007@gmail.com>
2024-10-08 08:47:55 -0300

    feat: read keyboard input

--- memori.c
+++ memori.c
@@ -1,7 +1,18 @@
-#include <stdio.h>
+#include <unistd.h>
 
 int main(int argc, char **argv) {
-    printf("Hello World!\n");
+    char c;
+
+    /* 
+        Read 1 byte from standart input.
+
+        By default, the terminal starts in canonical mode (cooked mode), which 
+        means that is required to press `ENTER` to send the input.
+
+        Until now, you need to press `CTRL-D` to send EOF or `CTRL-C` to 
+        terminate.
+    */
+    while (read(STDIN_FILENO, &c, 1) == 1);
 
     return 0;
 }
