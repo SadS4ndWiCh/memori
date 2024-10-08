aaed6d60684766db5a9cb430dc587c5d307c59f5
sads4ndwich <caiocamargo007@gmail.com>
2024-10-08 09:23:10 -0300

    feat: enable terminal raw mode

--- memori.c
+++ memori.c
@@ -1,6 +1,33 @@
 #include <unistd.h>
+#include <termios.h>
+
+void Terminal_enableRawMode(void) {
+    /*
+        Get the current state from terminal with `tcgetattr` and set to
+        `termios` struct to be able to manipule it.
+    */
+    struct termios terminal;
+    tcgetattr(STDIN_FILENO, &terminal);
+
+    /* 
+        `c_lflags` means `local flags` that describe other states:
+            - `ECHO` flag say to print each key you type to the terminal.
+        
+        To turn off some flag, you need to use bitwise operation. Just negate 
+        the `ECHO` flag.
+    */
+    terminal.c_lflag &= ~(ECHO);
+
+    /*
+        Set the modified state to the terminal. Using the `TCSAFLUSH` action, 
+        the modification is applied after all pending output to be written.
+    */
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &terminal);
+}
 
 int main(int argc, char **argv) {
+    Terminal_enableRawMode();
+
     char c;
 
     /* 
